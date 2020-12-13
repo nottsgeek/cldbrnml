7312e6843b0b500080595a03f83450f4dd371794
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-13 18:07:40 +0530

    Update anmlyapicall.sh

--- anmlyapicall.sh
+++ anmlyapicall.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-out=$(curl -X POST -H "Content-Type: application/json" -d '{"data": $1}' https://8pors5yzt1.execute-api.us-east-2.amazonaws.com/test/predictor)
+#out=$(curl -X POST -H "Content-Type: application/json" -d '{"data": $1}' https://8pors5yzt1.execute-api.us-east-2.amazonaws.com/test/predictor)
 
-y=`echo $out | jq '.scores[0].score'`
-z=1.4
+#y=`echo $out | jq '.scores[0].score'`
+#z=1.4
 
-if (( $(echo "$y > $z" |bc -l) )); then
+#if (( $(echo "$y > $z" |bc -l) )); then
  	echo "Anomaly detected"
-else
-	echo "All good"
-fi
+#else
+#	echo "All good"
+#fi
 
-echo $out
+#echo $out
+
+echo $1
