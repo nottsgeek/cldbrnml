7b18db1954e4c44f68ab319fb000057d3bc88322
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-16 13:04:51 +0530

    Update recvmsg.sh

--- recvmsg.sh
+++ recvmsg.sh
@@ -4,6 +4,7 @@ z=$(aws sqs receive-message --queue-url https://sqs.us-east-2.amazonaws.com/1693
 rcpthndle=`echo $z | jq -r '.Messages[0].ReceiptHandle'`
 if [ -z "$rcpthndle" ]; then
 	echo "something" > outfile
+	exit 0
 fi
 
 aws sqs delete-message --queue-url https://sqs.us-east-2.amazonaws.com/169302816586/sagemaker --receipt-handle ${rcpthndle} --region us-east-2
