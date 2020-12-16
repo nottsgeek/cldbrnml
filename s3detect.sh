2313a82fab8c5f80ec8646dabb0b21ca8fdde9b0
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-16 12:24:20 +0530

    Update s3detect.sh

--- s3detect.sh
+++ s3detect.sh
@@ -9,6 +9,7 @@ if (( $y > $z )); then
   change=$(curl -X POST -u $uname:$pass https://dev77787.service-now.com/api/now/table/incident)
   cnumber=`echo $change | jq '.result.number'`
   echo "High S3 Usage, Incident ${cnumber} raised and assigned." > cfile
+  aws sqs send-message --queue-url https://sqs.us-east-2.amazonaws.com/169302816586/sagemaker --message-body Abnormal --region us-east-2
 else
   echo "Normal S3 usage" > cfile
 fi
