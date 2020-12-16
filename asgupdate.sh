8e5e2f9abc19ac41aa06d53220e88a1c7da9640c
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-16 16:09:50 +0530

    Update asgupdate.sh

--- asgupdate.sh
+++ asgupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#aws autoscaling update-auto-scaling-group --auto-scaling-group-name sagemakerasglaunch --desired-capacity 1 --region us-east-2
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name sagemakerasglaunch --desired-capacity 1 --region us-east-2
 
 echo "New instance added to ASG sagemakerasglaunch" > cfile
