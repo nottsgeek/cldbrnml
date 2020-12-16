6771c56635f03824f868434216670ff18779f568
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-16 13:03:49 +0530

    Update recvmsg.sh

--- recvmsg.sh
+++ recvmsg.sh
@@ -2,9 +2,15 @@
 
 z=$(aws sqs receive-message --queue-url https://sqs.us-east-2.amazonaws.com/169302816586/sagemaker --region us-east-2)
 rcpthndle=`echo $z | jq -r '.Messages[0].ReceiptHandle'`
+if [ -z "$rcpthndle" ]; then
+	echo "something" > outfile
+fi
+
 aws sqs delete-message --queue-url https://sqs.us-east-2.amazonaws.com/169302816586/sagemaker --receipt-handle ${rcpthndle} --region us-east-2
 x=`echo $z | jq -r '.Messages[0].Body'`
 
 if [[ ${x} == "Abnormal" ]]; then
 	echo "Abormal" > outfile
+else
+	echo "something" > outfile
 fi
