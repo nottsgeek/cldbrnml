47532d778fd36f3a638f91a2ed83304760297d3d
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2020-12-16 16:06:35 +0530

    Update s3detect.sh

--- s3detect.sh
+++ s3detect.sh
@@ -3,10 +3,11 @@
 out=$(curl -X POST -H "Content-Type: application/json" -d '{"data": "'"$1"'"}' https://8pors5yzt1.execute-api.us-east-2.amazonaws.com/test/s3usage)
 
 y=`echo $out | jq '.predictions[0].score'`
-z=210000000
+z=210000
+
 echo $y
 a=${y%.*}
-if (( $a -gt $z )); then
+if [[ $a > $z ]]; then
   change=$(curl -X POST -u $uname:$pass https://dev77787.service-now.com/api/now/table/incident)
   cnumber=`echo $change | jq '.result.number'`
   echo "High S3 Usage, Incident ${cnumber} raised and assigned." > cfile
